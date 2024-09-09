faf94c1b9e0c5ee519892981c21c357d1ffd91ab
Gentuar <g.lushtaku@code-invention.com>
2024-09-09 23:45:38 +0200

    fixed

--- src/app/components/home/proyects/proyects.component.ts
+++ src/app/components/home/proyects/proyects.component.ts
@@ -5,41 +5,31 @@ import { AnalyticsService } from 'src/app/services/analytics/analytics.service';
 @Component({
   selector: 'app-proyects',
   templateUrl: './proyects.component.html',
-  styleUrls: ['./proyects.component.scss']
+  styleUrls: ['./proyects.component.scss'],
 })
 export class ProyectsComponent implements OnInit {
-
   customOptions: OwlOptions = {
     loop: true,
     mouseDrag: true,
     touchDrag: true,
     pullDrag: false,
-    navSpeed: 700,
+    navSpeed: 400,
     items: 1,
     autoplay: true,
-    autoplayTimeout:3000
-  }
+    autoplayTimeout: 3000,
+  };
 
   @ViewChild('imgContainer') imgContainer: ElementRef;
 
+  constructor(public analyticsService: AnalyticsService) {}
 
-  constructor(
-    public analyticsService: AnalyticsService
-  ) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-
-
-
+  debug() {
+    this.imgContainer.nativeElement.scroll({
+      top: this.imgContainer.nativeElement.scrollHeight,
+      left: 0,
+      behavior: 'smooth',
+    });
   }
-
-debug(){
-
-  this.imgContainer.nativeElement.scroll({
-    top: this.imgContainer.nativeElement.scrollHeight,
-    left: 0,
-    behavior: 'smooth',    
-  });
-}
-
 }
